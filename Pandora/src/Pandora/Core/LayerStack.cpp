bb7ce206fb5e180e2e87115dfb64b4f765f3fe7c
WhoseTheNerd <whosethenerd@protonmail.com>
2023-04-06 13:10:12 +0300

    call Layer::OnDetach() before deleting it

--- Pandora/src/Pandora/Core/LayerStack.cpp
+++ Pandora/src/Pandora/Core/LayerStack.cpp
@@ -8,8 +8,10 @@ namespace Pandora {
 
 	LayerStack::~LayerStack()
 	{
-		for (Layer* layer : m_Layers)
+		for (Layer* layer : m_Layers) {
+            layer->OnDetach();
 			delete layer;
+        }
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
