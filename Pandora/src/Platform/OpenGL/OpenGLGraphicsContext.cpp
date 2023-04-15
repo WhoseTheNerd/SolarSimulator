c283309080ec33baf30e1d141944b52520da0128
WhoseTheNerd <whosethenerd@protonmail.com>
2023-04-15 10:32:26 +0300

    Added culling

--- Pandora/src/Platform/OpenGL/OpenGLGraphicsContext.cpp
+++ Pandora/src/Platform/OpenGL/OpenGLGraphicsContext.cpp
@@ -87,6 +87,10 @@ namespace Pandora {
 
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_MULTISAMPLE); 
+
+        glEnable(GL_CULL_FACE);  
+        glCullFace(GL_BACK);  
+        glFrontFace(GL_CCW); 
     }   
 
     void OpenGLGraphicsContext::SwapBuffers()
